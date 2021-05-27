b4954d02edfa70b0083f1dc0587f12cfc1c9d3fc
Florian Pisani <me@fpisani.dev>
2021-05-27 21:39:34 +0200

    Test bootstraping define purposes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,9 @@
 $loader = require __DIR__ .'/../vendor/autoload.php';
 $loader->addPsr4('Kraber\\Test\\', __DIR__);
 
+/**
+ * Used by integration tests (php-http/psr7-integration-tests)
+ */
 define('URI_FACTORY', \Kraber\Http\Factory\UriFactory::class);
 define('STREAM_FACTORY', \Kraber\Http\Factory\StreamFactory::class);
 define('UPLOADED_FILE_FACTORY', \Kraber\Http\Factory\UploadedFileFactory::class);
